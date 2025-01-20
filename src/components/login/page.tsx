7c8b9e25f1c6ad95915869303feb0302cf9408ed
adeka stanley <adekastanley2@gmail.com>
2025-01-20 15:33:32 +0100

    fix

--- src/components/login/page.tsx
+++ src/components/login/page.tsx
@@ -11,7 +11,7 @@ export default function LoginPage() {
 						<div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
 							<span>Q</span>
 						</div>
-						upid
+						upid!
 					</a>
 				</div>
 				<div className="flex flex-1 items-center justify-center">
