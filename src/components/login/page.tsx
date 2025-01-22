0ebb782915b32720ac590399d666d8c681400697
adeka stanley <adekastanley2@gmail.com>
2025-01-22 18:52:27 +0100

    progress

--- src/components/login/page.tsx
+++ src/components/login/page.tsx
@@ -1,18 +1,19 @@
 // import { GalleryVerticalEnd } from "lucide-react";
+import siteIcons from "@/assets/icons/siteIcons";
 
 import { LoginForm } from "@/components/login-form";
+import { Link } from "react-router-dom";
 
 export default function LoginPage() {
 	return (
-		<div className="grid min-h-svh lg:grid-cols-2">
+		<div className="grid min-h-svh lg:grid-cols-2 bg-secondary">
 			<div className="flex flex-col gap-4 p-6 md:p-10">
-				<div className="flex justify-center gap-2 md:justify-start">
-					<a href="#" className="flex items-center  font-medium">
-						<div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-							<span>Q</span>
+				<div className="flex justify-center gap-2 md:justify-start pt-">
+					<Link to="/" className="flex items-center  font-medium">
+						<div className="flex  w-20 items-center justify-center rounded-md  text-primary-foreground">
+							<img src={siteIcons.logo2} alt="qupid logo" />
 						</div>
-						upid!
-					</a>
+					</Link>
 				</div>
 				<div className="flex flex-1 items-center justify-center">
 					<div className="w-full max-w-xs">
