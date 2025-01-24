9705b98e94d23d8429155e1c4081f2e1d1451344
adeka stanley <adekastanley2@gmail.com>
2025-01-24 13:56:25 +0100

    update

--- src/components/pages/One.tsx
+++ src/components/pages/One.tsx
@@ -1,7 +1,9 @@
 import { Button } from "../ui/button";
 import backgrounds from "@/assets/backgrounds/backgrounds";
+import { useIsMobile } from "@/hooks/use-mobile";
 
 const One = () => {
+	const isMobile = useIsMobile();
 	return (
 		<div className=" min-h-screen relative bg-black">
 			<section className="w-full h-full flex  pt-40 flex-col items-center gap-5 bg-transparent">
@@ -28,9 +30,15 @@ const One = () => {
 					ducimus similique omnis?
 				</p>
 			</section>
-			<section className="flex">
-				<div></div>
-				<div></div>
+			<section className="flex flex-col sm:flex-row  gap-5 sm:px-20">
+				<div className="w-full h-full">
+					<div className=" h-[20rem]  bg-[#EF3054] w-full"></div>
+					{!isMobile && <div className=" h-[20rem]  bg-[#EF3054] w-full"></div>}
+				</div>
+				<div className="w-full h-full flex flex-col max-sm:gap-5">
+					<div className="h-[20rem] bg-[#FF715B] w-full"></div>
+					<div className="h-[20rem] bg-[#1EA896] w-full"></div>
+				</div>
 			</section>
 		</div>
 	);
